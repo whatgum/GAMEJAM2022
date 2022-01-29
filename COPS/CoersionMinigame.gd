a3b6016376fb6c5b5fb59b8089154d9280ce6f4d
Baerett Stone <baerstone3@gmail.com>
2022-01-29 12:34:29 -0500

    Tiny changes on coersion, pushing to solve merge

--- COPS/CoersionMinigame.gd
+++ COPS/CoersionMinigame.gd
@@ -23,9 +23,10 @@ var susDecayMult : float = 1 # Number goes down the more the player sweettalks
 var susIncreaseMin : float = 10
 var susIncreaseMax : float = 25
 
-var susBar
-var truthChanceLabel
-# decimal between 0.0 and 1.0. Probability of success for Asking Nicely
+var susBar : ProgressBar
+var truthChanceLabel : RichTextLabel
+@export var chanceRamp : Gradient 
+# decimal between 0 and 100. Probability of success for Asking Nicely
 var realChance : float = 0
 var chanceIncreaseMax : float = 25
 
@@ -46,22 +47,36 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	updateGUI(delta)
 	pass
 
 func sweetTalk():
 	# increase sus
 	var susRoll = randf_range(susIncreaseMin, susIncreaseMax)
+	print(susRoll)
 	suspicion += susRoll
-	
+	print(suspicion)
 	# increase chances. scale chance increase with how high the sus changed
 	var chanceModifier = (
 		susRoll - susIncreaseMin)/(
 			susIncreaseMax-susIncreaseMin)
 			
 	var chanceIncrease = chanceIncreaseMax*chanceModifier
+	print(chanceIncrease)
 	realChance += chanceIncrease
-
+	print(realChance)
 	# decrease sus decay speed
 	# TODO resuming this in the morning
 
-	
+# little helper function where you pass in a number between 0 and 1.0, and it
+# will return a color more red as it gets closer to 0, and more green as it
+# gets closer to 1. Using this so it visually shows how decent your chances
+# are
+func getColorByPercent(percent):
+	pass
+
+func updateGUI(delta):
+	susBar.value = suspicion
+	truthChanceLabel.text = str(int(realChance)) + "%"
+	truthChanceLabel.modulate = chanceRamp.interpolate(realChance/100)
+
