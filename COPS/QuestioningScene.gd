c7713cc3f7a930ad4a104844210252355632a8a1
palmerjw <palmerjw@iu.edu>
2022-01-29 14:02:03 -0500

    evidence exists now

--- COPS/QuestioningScene.gd
+++ COPS/QuestioningScene.gd
@@ -2,9 +2,13 @@ extends Control
 
 var currentEvidence = ""
 var currentQuestion = ""
+
+var texture = load("res://icon.png")
 signal startConversation(detectiveDict, interviewDict, key)
 signal pressAtEnd()
 signal addQuestion(question, pressable)
+signal addEvidence(texture,title, description)
+
 
 func _process(delta):
 	if(Input.is_action_just_pressed("removeEvidence")):
@@ -15,7 +19,7 @@ func _process(delta):
 
 func _on_button_pressed():
 	if(currentEvidence != ""):
-		print("Ask about : " + currentEvidence)
+		_on_question_container_start_dialogue(currentEvidence, false)
 	Input.set_custom_mouse_cursor(null)
 
 # Purpose : This dictionary uses questions asked as keys
@@ -25,6 +29,7 @@ func _on_button_pressed():
 var ConversationDictionary := {
 	"THE QUICK BROWN FOX JUMPS OVER THE SMALL DOG?" : [{"THE QUICK BROWN FOX JUMPS OVER THE SMALL DOG?" : "Whats all this about a fox??"},{"Whats all this about a fox??" : "yeah it was a fox, I swear"}]}
 
+
 # Purpose : This dictionary uses questions asked as keys
 #			as values it holds arrays consisting of arrays
 #			These inner arrays stores questions to be asked and whether said question is pressable
@@ -33,6 +38,14 @@ var questionDictioary := {
 	"THE QUICK BROWN FOX JUMPS OVER THE SMALL DOG?" : [["question?", false], ["whats a question", true]]
 }
 
+# Purpose : This dictionary uses questions asked as keys
+#			as values it holds arrays consisting of arrays
+#			these inner arrays stores evidence texture, the title of evidence and the description of evidence
+#			These inner arrays are used to create new evidence scenes for the player to view
+var evidenceDictionary := {
+	"THE QUICK BROWN FOX JUMPS OVER THE SMALL DOG?" : 
+		[[texture,"I think that I love you", "The mater of love is not a trival thing. But they professed it to you."]]
+}
 
 
 func _on_question_container_start_dialogue(key : String, pressable : bool):
@@ -45,9 +58,15 @@ func _on_question_container_start_dialogue(key : String, pressable : bool):
 		emit_signal("startConversation", ConversationDictionary[key][0], ConversationDictionary[key][1], key )
 
 
+
 func _on_dialog_manager_add_questions():
 	if(questionDictioary.has(currentQuestion)):
 		var arrayOfQuestions = questionDictioary[currentQuestion]
 		for i in arrayOfQuestions:
 			emit_signal("addQuestion",i[0], i[1])
 		questionDictioary.erase(currentQuestion)
+	if(evidenceDictionary.has(currentQuestion)):
+		var arrayOfEvidence = evidenceDictionary[currentQuestion]
+		for i in arrayOfEvidence:
+			emit_signal("addEvidence", i[0], i[1], i[2])
+		arrayOfEvidence.erase(currentQuestion)
