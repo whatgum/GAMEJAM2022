c22698e85d205193d5cea3deec47f1edbdeec8d3
palmerjw <palmerjw@iu.edu>
2022-01-29 18:59:28 -0500

    honestly confused on how to proceed next

--- COPS/scales_of_justice.gd
+++ COPS/scales_of_justice.gd
@@ -1,6 +1,12 @@
 extends Control
 
+var evidenceBlock = load("res://evidenceBlock.tscn")
+
 # Called when the node enters the scene tree for the first time.
-func _init(arrayOfEvidence : Array):
+func init(arrayOfEvidence : Array):
 	for i in arrayOfEvidence:
-		pass
+		var newBlock = evidenceBlock.instantiate()
+		newBlock.init(i._texture, i._text, i._text2)
+		get_child(1).add_child(newBlock)
+		newBlock.global_position = get_child(3).global_position
+
