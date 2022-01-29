0afc741ee515b59f333a13642d2eeceaa0b1b9cf
palmerjw <palmerjw@iu.edu>
2022-01-29 10:27:25 -0500

    Better evidence

--- COPS/evidenceScene.gd
+++ COPS/evidenceScene.gd
@@ -1,9 +1,10 @@
 extends Panel
 
 # Purpose : sets up the scene
-func init(texture : Texture, string : String) -> void:
+func init(texture : Texture, string : String, string2 : String) -> void:
 	get_child(0).texture = texture
 	get_child(1).text = string
+	get_child(3).get_child(0).text = string2
 
 
 func _on_texture_button_pressed():
